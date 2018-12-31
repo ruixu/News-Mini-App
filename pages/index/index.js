7e863ac20fd9becd3bc07bb0f6ef2d78af9aa5d4
Revo <xurui91@gmail.com>
2018-12-31 15:30:44 -0800

    build a wechat news miniapp

---
+++ pages/index/index.js
@@ -0,0 +1,109 @@
+var moment = require('../../libs/moment.min.js');
+
+const categoryMap = {
+  'gn': '国内',
+  'gj': '国际',
+  'cj': '财经',
+  'yl': '娱乐',
+  'js': '军事',
+  'ty': '体育',
+  'other': '其他',
+}
+
+Page({
+  data: {
+    topNews:[],
+    newsList: [],
+    category: 'gn',
+    categoryList: [
+      { 'en': 'gn', 'cn': '国内' }
+    ],
+  },
+
+  onLoad() {
+    this.setCategory()
+    this.getNews()
+  },
+
+  setCategory() {
+    let categoryList = []
+    for (var key in categoryMap) {
+      categoryList.push({
+        en: key,
+        cn: categoryMap[key]
+      })
+    }
+    this.setData({
+      categoryList: categoryList
+    })
+  },
+
+  getNews(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: this.data.category
+      },
+      success: res => {
+        let result = res.data.result
+        this.setNewsList(result)     
+      },
+      fail: res => {
+        console.log(res)
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  },
+
+  setNewsList(result) {
+    let topNews = []
+    let newsList = []
+
+    // get top News
+    topNews.push({
+      id: result[0].id,
+      title: result[0].title.slice(0, 30),
+      time: moment(result[0].date).fromNow(),
+      source: result[0].source || '快读资讯',
+      firstImage: result[0].firstImage || "/images/default-news-img.png",
+    })
+
+    // get following News
+    for (let i = 1; i < result.length; i += 1) {
+      newsList.push({
+        id: result[i].id,
+        title: result[i].title.slice(0, 30),
+        time: moment(result[i].date).fromNow(),
+        source: result[i].source || '快读资讯',
+        firstImage: result[i].firstImage || "/images/default-news-img.png",
+      })
+    }
+    this.setData({
+      newsList: newsList,
+      topNews: topNews
+    })
+  },
+
+  onTapCategory(event) {
+    this.setData({
+      category: event.currentTarget.dataset.category
+    })
+    this.getNews()
+  },
+
+  onTapNews(event) {
+    let newsID = event.currentTarget.dataset.newsid
+    wx.navigateTo({
+      url: '/pages/news/news?id=' + newsID
+    })
+  },
+
+  onPullDownRefresh() {   
+    this.getNews(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+
+})
---
+++ pages/news/news.js
@@ -0,0 +1,80 @@
+var moment = require('../../libs/moment.min.js');
+
+Page({
+  data: {
+    id: '',
+    newsInfo: {}
+  },
+
+  onLoad(opt) {
+    this.setData({
+      id: opt.id
+    })
+    this.getDetail()
+  },
+
+  // get details of the news
+  getDetail(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/detail',
+      data: {
+        id: this.data.id
+      },
+      success: res => {
+        let newsInfo = res.data.result 
+        newsInfo.time = moment(newsInfo.date).format('MMMM Do YYYY, h:mm:ss'),
+        this.setData({
+          newsInfo: newsInfo,
+        })
+
+        let fields = this.getNewsContent(res.data.result.content)
+        this.setData({
+          newsContent: fields
+        })
+      },
+      fail: res => {
+        console.log(res)
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  },
+
+  // get contents of the news
+  getNewsContent(content) {
+    let fields = []
+    for (let i = 0; i < content.length; i += 1) {
+      if (content[i].type === 'image') {
+        fields.push([{
+          name: 'img',
+          attrs: {
+            class: 'article-img',
+            src: content[i].src
+          }
+        }])
+      }
+      else {
+        fields.push([{
+          name: content[i].type,
+          attrs: {
+            class: 'article-text'
+          },
+          children: [{
+            type: 'text',
+            text: content[i].text
+          }]
+        }
+        ])
+      }
+    }
+    return fields;
+  },
+
+  onPullDownRefresh() {  
+    this.getArticle(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+
+})
